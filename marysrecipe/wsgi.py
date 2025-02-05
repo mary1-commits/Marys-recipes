ca1bba66c7e47d27000ca5ff9839b8507f6364ec
mary1-commits aden <maryansaeed67@gmail.com>
2025-02-05 13:11:53 +0000

    change codestar to marysrecipe

--- marysrecipe/wsgi.py
+++ marysrecipe/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for codestar project.
+WSGI config for marysrecipe project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marysrecipe.settings')
 
 application = get_wsgi_application()
