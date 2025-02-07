af6462d4ff57be20e72f4eb08c7f174e754cdac3
mary1-commits aden <maryansaeed67@gmail.com>
2025-02-07 12:53:39 +0000

    adjust post list andd delete individual html for recipes

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,12 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+    context_object_name = "posts"
 
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
 
 def post_detail(request, slug):
     """
