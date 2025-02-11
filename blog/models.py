773bc692fae7aa8fdd229d53199fe3bd4e1bf03c
Maryan <maryansaeed67@gmail.com>
2025-02-11 10:45:31 +0000

    reverse models.py to fix server 500 issue

--- blog/models.py
+++ blog/models.py
@@ -4,58 +4,37 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Post Model
+# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+    User, on_delete=models.CASCADE, related_name="blog_posts"
+)
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
-
-    class Meta:
+    updated_on = models.DateTimeField(auto_now=True)
+    field_2 = models.CharField(default='Hello, World!')
+    field_3 = models.CharField(null=True)
+class Meta:
         ordering = ["-created_on"]
 
-    def __str__(self):
+def __str__(self):
         return f"{self.title} | written by {self.author}"
 
-# Comment Model with CRUD Methods
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
-    )
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
+class Meta:
         ordering = ["created_on"]
 
-    def __str__(self):
-        return f"Comment by {self.author} on {self.post.title}"
-
-    # CRUD Methods for Comments
-    @classmethod
-    def create_comment(cls, post, author, body):
-        """Creates a new comment"""
-        comment = cls(post=post, author=author, body=body)
-        comment.save()
-        return comment
-
-    def update_comment(self, new_body):
-        """Updates the comment"""
-        self.body = new_body
-        self.save()
-        return self
-
-    def delete_comment(self):
-        """Deletes the comment"""
-        self.delete()
+def __str__(self):
+        return f"Comment {self.body} by {self.author}"
