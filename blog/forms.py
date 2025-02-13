bffe68e5ab177b0ccce6c68c84c29ebc2046dfaf
Maryan <maryansaeed67@gmail.com>
2025-02-13 16:01:10 +0000

    add post form to forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,28 @@
-from .models import Comment
+from .models import Comment, Post
 from django import forms
 
 
+
 class CommentForm(forms.ModelForm):
+
     class Meta:
         model = Comment
-        fields = ('body',)
+        fields = ['body']
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 4}),
+        }
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'content',
+            'featured_image',
+            'excerpt',
+        ]
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 10}),
+            'excerpt': forms.Textarea(attrs={'rows': 3}),
+        }
