4bab61e30c9c9f0da793a54b2a6f211e989bcd30
Maryan <maryansaeed67@gmail.com>
2025-02-13 15:50:56 +0000

    update about html to include hero image and adjust my styling

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic, View
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Post, Comment
 from .forms import CommentForm
 
@@ -12,29 +13,22 @@ class PostList(generic.ListView):
     paginate_by = 6
     context_object_name = "posts"
 
-# Detail View for a Single Blog Post
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = "blog/post_detail.html"
-    context_object_name = "post"
-
-# Function-Based View for Blog Post Details & Comments
+# Detail View for a Single Blog Post with Comments
 def post_detail(request, slug):
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+    post = get_object_or_404(Post, slug=slug, status=1)
     comments = post.comments.filter(approved=True).order_by("-created_on")
-    comment_count = post.comments.filter(approved=True).count()
+    comment_count = comments.count()
     comment_form = CommentForm()
 
     if request.method == "POST":
-        comment_form = CommentForm(data=request.POST)
+        comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.author = request.user
             comment.post = post
             comment.save()
-            messages.success(request, 'Comment submitted and awaiting approval!')
-            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+            messages.success(request, "Comment submitted and awaiting approval!")
+            return redirect("post_detail", slug=slug)
 
     return render(
         request,
@@ -47,38 +41,23 @@ def post_detail(request, slug):
         }
     )
 
-# Create Comment View
-class CommentCreateView(View):
-    def post(self, request, slug):
-        post = get_object_or_404(Post, slug=slug)
-        comment_form = CommentForm(data=request.POST)
-        
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.author = request.user
-            comment.post = post
-            comment.save()
-            messages.success(request, 'Comment submitted and awaiting approval!')
-        else:
-            messages.error(request, 'Error submitting comment!')
-
-        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-
 # Edit Comment View
 def comment_edit(request, slug, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id, author=request.user)
-    
+
     if request.method == "POST":
         comment_form = CommentForm(request.POST, instance=comment)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
-            comment.approved = False  # Requiring re-approval after edits
+            comment.approved = False  # Require re-approval after edits
             comment.save()
-            messages.success(request, 'Comment updated successfully!')
+            messages.success(request, "Comment updated successfully!")
         else:
-            messages.error(request, 'Error updating comment!')
+            messages.error(request, "Error updating comment!")
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        return redirect("post_detail", slug=slug)
+
+    return render(request, "blog/edit_comment.html", {"comment_form": CommentForm(instance=comment)})
 
 # Delete Comment View
 def comment_delete(request, slug, comment_id):
@@ -86,11 +65,10 @@ def comment_delete(request, slug, comment_id):
 
     if request.method == "POST":
         comment.delete()
-        messages.success(request, 'Comment deleted successfully!')
+        messages.success(request, "Comment deleted successfully!")
+        return redirect("post_detail", slug=slug)
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-
-# Dynamic Recipe Detail View (For all Somali recipes)
+# Recipe Detail View (For Somali Recipes)
 def recipe_detail(request, slug):
-    recipe = get_object_or_404(Post, slug=slug, status=1)  # Assuming recipes are also Posts
+    recipe = get_object_or_404(Post, slug=slug, status=1)
     return render(request, "blog/post_detail.html", {"post": recipe})
