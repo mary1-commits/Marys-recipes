97d246e10e4ec10300ca8ed591949676eff1ad7c
Maryan <maryansaeed67@gmail.com>
2025-02-13 16:05:45 +0000

    add delete models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -7,23 +8,31 @@ STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
+    User, on_delete=models.CASCADE, related_name="blog_posts")
     featured_image = CloudinaryField('image', default='placeholder')
-    content = models.TextField()
+    content = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    field_2 = models.CharField(default='Hello, World!')
-    field_3 = models.CharField(null=True)
-class Meta:
-        ordering = ["-created_on"]
+    field_2 = models.CharField(max_length=255, default='Hello, World!')
+    field_3 = models.CharField(max_length=255, null=True, blank=True)
 
-def __str__(self):
-        return f"{self.title} | written by {self.author}"
+
+    class Meta:
+            ordering = ["-created_on"]
+
+    def __str__(self):
+            return f"{self.title} | written by {self.author}"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -33,8 +42,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-class Meta:
+
+    class Meta:
         ordering = ["created_on"]
 
-def __str__(self):
+    def __str__(self):
         return f"Comment {self.body} by {self.author}"
