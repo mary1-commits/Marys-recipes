2e4ec465829bd12740bdc81390bbdd0c0e3c79e0
Maryan <maryansaeed67@gmail.com>
2025-02-17 15:55:26 +0000

    adjust debug to false

--- marysrecipe/settings.py
+++ marysrecipe/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'authentic-recipe-457331f032d1.herokuapp.com',
